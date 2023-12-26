1d49e5f771525e343b9c232d901b83f920afab0c
Christophe Bral <92055523+Philopitre@users.noreply.github.com>
2023-12-27 00:38:56 +0100

    initial commit

--- Javascript/JavaScript Algorithms and Data Structures (Beta)/Learn Basic JavaScript by buildung role playing game/script.js
+++ Javascript/JavaScript Algorithms and Data Structures (Beta)/Learn Basic JavaScript by buildung role playing game/script.js
@@ -142,7 +142,7 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
-  } else {
+    }else {
     text.innerText = "You already have the most powerful weapon!";
     button2.innerText = "Sell weapon for 15 gold";
     button2.onclick = sellWeapon;
