cc4a0626f2b19826b8e9def5575f053125e5dd76
Christophe Bral <92055523+Philopitre@users.noreply.github.com>
2024-01-02 17:07:20 +0100

    playPreviousSong updated

--- Javascript/JavaScript Algorithms and Data Structures (Beta)/Learn Basic String and Array Methods by Building a music Player/script.js
+++ Javascript/JavaScript Algorithms and Data Structures (Beta)/Learn Basic String and Array Methods by Building a music Player/script.js
@@ -122,8 +122,14 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () => {
+const playPreviousSong = () =>{
+  if (userData?.currentSong === null) return;
+  else {
+  const currentSongIndex = getCurrentSongIndex();
+  const previousSong = userData?.songs[currentSongIndex - 1];
 
+  playSong(previousSong.id);
+  }
 };
 
 const renderSongs = (array) => {
